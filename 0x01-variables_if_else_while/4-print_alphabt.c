835341d73e802fb5e77000663e617d7482a6cd18
Mohammed881939 <mohammed881939@gmail.com>
2023-07-17 17:19:05 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main -  Entry point
  *
- * Description: print all alphabet letters except q and e
+ * Description: print all aplhabet letters except q and e
  *
  * Return: Always 0 (Success)
  */
