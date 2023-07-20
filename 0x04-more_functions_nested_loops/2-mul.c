fee7a7226975741640e8c43a696d58a64572e63c
Mohammed881939 <mohammed881939@gmail.com>
2023-07-20 11:20:51 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _mul - mutiplies tow integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+ */
+
+int _mul(int a, int b)
+{
+	return (a * b);
+}
