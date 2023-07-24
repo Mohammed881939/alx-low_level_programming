2c5e3f80a4dfb59116d28d52ce8f9daeeffb223f
Mohammed881939 <mohammed881939@gmail.com>
2023-07-24 13:49:36 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int = n;
 
-	n = 402
-	printf("n=%d\n", n);
+	n = 402;
+	_putchar("n=%d\n", n);
 	reset_to_98(&n);
-	printf("n=%d\n", n);
+	_putchar("n=%d\n", n);
 	return (0);
 }
