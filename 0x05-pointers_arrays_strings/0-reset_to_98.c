7e510628d6c0a6f3d51a19967126e6f9f12b4667
Mohammed881939 <mohammed881939@gmail.com>
2023-07-24 14:53:59 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * reset_to_98 - prints n as pointer
+ * reset_to_98 - print n as pointer to 98
  *
- * @n: pointer
+ * @n:Aninteger input pointer
  *
- * Return: Always 0.
+ * Return: empty
  */
 void reset_to_98(int *n)
 {
 	int n;
 
-	*n = 98
 	n = 402;
 	_putchar("n=%d\n", n);
 	reset_to_98(&n);
