b47635cdbb1f36e26224d1fda87fa020ee5f8818
Mohammed881939 <mohammed881939@gmail.com>
2023-07-24 13:23:39 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stido.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int = n;
+
+	n = 402
+	_puchar("n=%d\n", n);
+	reset_to_98(&n);
+	_putchar("n=%d\n", n);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+#endif /*MAIN_H*/
