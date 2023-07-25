e9a44e1b00cf67d95ef4ec30e6af25991741cc3d
Mohammed881939 <mohammed881939@gmail.com>
2023-07-24 17:34:22 -0700

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_rev - Print a string, in reverse followed by a new line.
+ * _puts - Print a string, followed by a new line, to stdout.
  * @str: Type char pointer
  * Return: Always 0.
  */
-void print_rev(char *str)
+void _puts(char *str)
 {
 	int a;
 	int let;
 
 	for (a = 0; str[a] != 0; a++)
-	{
-	}
-	for  (a = a - 1; a >= 0; a--)
 	{
 		let = str[a];
 		_putchar(let);
