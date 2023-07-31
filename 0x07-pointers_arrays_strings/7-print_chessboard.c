450c47ea9bee1cb5685fa059a50522d9e139fd30
Mohammed881939 <mohammed881939@gmail.com>
2023-07-31 15:09:33 -0700

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * print_chessboard - Funtion that prints the chessboard.
+ * @a:type char str
  * Return: Always 0.
  */
-int main(void)
+void print_chessboard(char (*a)[8])
 {
-    char board[8][8] = {
-        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-    };
-    print_chessboard(board);
-    return (0);
+	int x, y;
+
+	x = 0;
+	while (x < 8)
+	{
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
 }
